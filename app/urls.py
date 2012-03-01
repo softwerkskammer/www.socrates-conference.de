09090563a8740607b94b53a281fa4e398c02248e
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-03-01 22:10:36 +0100

    added redirect to wiki from index

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'app.views.home', name='home'),
     # url(r'^app/', include('app.foo.urls')),
 
+    url(r'^$', redirect_to, {'url': '/wiki/', 'permanent': False}),
     url(r'', include('social_auth.urls')),
 
     url(r'^wiki/', include('wakawaka.urls')),
