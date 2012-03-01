d47a82e8dd2e9ff18890c2cf417ad7938f0723e6
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-03-02 00:37:39 +0100

    minor changes for making app ep.io ready

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -1,2 +1,15 @@
 from __future__ import absolute_import
 from .base import *
+
+from bundle_config import config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config['postgres']['database'],
+        'USER': config['postgres']['username'],
+        'PASSWORD': config['postgres']['password'],
+        'HOST': config['postgres']['host'],
+    }
+}
+
+MEDIA_ROOT = config['core']['data_directory']
