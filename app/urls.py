9c9e1f89ee37364da26f09463b75c67dde45135e
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-03-05 17:45:49 +0100

    imported static homepage version into current style

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^app/', include('app.foo.urls')),
-
-    url(r'^$', redirect_to, {'url': '/wiki/', 'permanent': False}, name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'', include('social_auth.urls')),
 
     url(r'^wiki/', include('wakawaka.urls')),
