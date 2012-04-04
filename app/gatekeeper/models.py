5985f76d436b563ab16583b17c251043a88e70fd
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-04-04 23:24:31 +0200

    renamed post-twitter handler function to be more specific

--- app/gatekeeper/models.py
+++ app/gatekeeper/models.py
@@ -6,28 +6,36 @@ from social_auth.backends.twitter import TwitterBackend
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+import logging
 
-def new_users_handler(sender, user, response, details, **kwargs):
+
+logger = logging.getLogger(__name__)
+
+
+def new_socialauth_users_handler(sender, user, response, details, **kwargs):
     user.is_active = False
     send_moderation_notices(user)
     return False
 
-socialauth_registered.connect(new_users_handler, sender=None)
+socialauth_registered.connect(new_socialauth_users_handler, sender=None)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
+        logger.debug('Creating profile for new user "%s".' % instance.username)
         UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
 
 
 def twitter_extra_values(sender, user, response, details, **kwargs):
+    logger.debug('Saving twitter profile for user "%s".' % user.username)
     profile = user.get_profile()
     profile.twitter_name = response.get("screen_name")
     profile.blog_url = response.get("url") or ""
     profile.location = response.get("location") or ""
     profile.save()
+    logger.debug('Saving twitter profile for user "%s". DONE!' % user.username)
     return True
 
 pre_update.connect(twitter_extra_values, sender=TwitterBackend)
@@ -41,3 +49,6 @@ class UserProfile(models.Model):
     focus = models.CharField(max_length=255, blank=True)
     twitter_name = models.CharField(max_length=40, blank=True)
     blog_url = models.URLField(blank=True, verify_exists=False)
+    
+    def __unicode__(self):
+        return u'<UserProfile "%s"' % self.__dict__
