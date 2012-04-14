5469538dd0edf50fe9ddffd7d4f2647b8a800adf
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-04-14 20:10:22 +0200

    fixed missing error msg when no or wrong credentials were given on login

--- app/gatekeeper/auth_backend.py
+++ app/gatekeeper/auth_backend.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.models import User
 
-
+import logging
+logger = logging.getLogger(__name__)
 class EmailModelBackend(object):
     def authenticate(self, username=None, password=None):
         try:
             user = User.objects.get(email=username)
+            logger.debug("EMAILBACKEND 2 %s" % user)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
-            pass
-        return None
+            return None
 
     def get_user(self, user_id):
         try:
